2356d67c541b18bc6edcef0ab6d9783ddb7f91d6
ns-26 <56130142+ns-26@users.noreply.github.com>
2020-11-19 18:27:13 +0530

    add code for uploading user images

--- controllers/api/v1/userController.js
+++ controllers/api/v1/userController.js
@@ -31,34 +31,52 @@ module.exports.login = async function (req, res) {
 
 module.exports.create = async function (req, res) {
   try {
-    if (req.body.password != req.body.confirm_password) {
-      return res
-        .status(403)
-        .json({ message: 'Password and Confirm Password does not match' });
-    }
-    let user = await User.findOne({ email: req.body.email });
-    if (!user) {
-      let newUser = await User.create(req.body);
-      if (newUser) {
-        let filterUser = await User.findOne({ email: req.body.email })
-          .select('-password')
-          .select('-__v');
-        return res.status(200).json({
-          message: 'Sign In successful',
-          data: {
-            token: jwt.sign(filterUser.toJSON(), 'something', {
-              expiresIn: '1000000',
-            }),
-          },
-        });
-      } else {
-        return res.status(500).json({
-          message: 'User not registered',
-        });
+    User.uploadedAvatar(req, res, async function (err) {
+      if (err) {
+        console.log('Multer Error', err);
+        res.status(500).json({ message: 'Error Uploading Image' });
       }
-    } else {
-      return res.status(403).json({ message: 'Username Exists' });
-    }
+      if (req.body.password != req.body.confirm_password) {
+        return res
+          .status(403)
+          .json({ message: 'Password and Confirm Password does not match' });
+      }
+      let user = await User.findOne({ email: req.body.email });
+      if (!user) {
+        let newUser = await User.create(req.body);
+        if (newUser) {
+          if (req.file) {
+            if (newUser.avatar) {
+              if (fs.existsSync(path.join(__dirname, '..', 'newUser.avatar'))) {
+                fs.unlinkSync(path.join(__dirname, '..', 'newUser.avatar'));
+              }
+            }
+
+            console.log(req.file);
+            //this is saving the path of the uploaded file into the avatar field of the user
+            newUser.avatar = User.AvatarPath + '/' + req.file.filename;
+          }
+          newUser.save();
+          let filterUser = await User.findOne({ email: req.body.email })
+            .select('-password')
+            .select('-__v');
+          return res.status(200).json({
+            message: 'Sign In successful',
+            data: {
+              token: jwt.sign(filterUser.toJSON(), 'something', {
+                expiresIn: '1000000',
+              }),
+            },
+          });
+        } else {
+          return res.status(500).json({
+            message: 'User not registered',
+          });
+        }
+      } else {
+        return res.status(403).json({ message: 'Username Exists' });
+      }
+    });
   } catch (err) {
     console.log(err);
     return res.status(500).json({
--- modals/User.js
+++ modals/User.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
+const multer = require('multer');
+
+const path = require('path');
+
+const AVATAR_PATH = path.join('/uploads/users/avatar');
+
 const userSchema = new mongoose.Schema(
   {
     email: {
@@ -25,11 +31,30 @@ const userSchema = new mongoose.Schema(
         ref: 'Event',
       },
     ],
+    avatar: {
+      type: String,
+    },
   },
   {
     timestamps: true,
   }
 );
+
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, '..', AVATAR_PATH));
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now());
+  },
+});
+
+//static functions
+userSchema.statics.uploadedAvatar = multer({ storage: storage }).single(
+  'avatar'
+);
+
+userSchema.statics.AvatarPath = AVATAR_PATH;
 
 //modal creation
 const User = mongoose.model('User', userSchema);
